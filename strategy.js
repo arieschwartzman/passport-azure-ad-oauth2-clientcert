899cde0a6b6f557ea69332dddae5cc96ebb1c64c
Arie Schwartzman <ariesch@microsoft.com>
2020-09-04 19:18:02 +0300

    get fingerprint  from pem with crypto

--- strategy.js
+++ strategy.js
@@ -2,6 +2,7 @@
 var util = require('util')
 ,OAuth2Strategy = require('passport-azure-ad-oauth2')
 ,jwt = require('jsonwebtoken')
+,crypto = require("crypto")
 
 function Strategy (options, verify) {
   options = options || {};
@@ -9,7 +10,6 @@ function Strategy (options, verify) {
   this.name = 'azure_ad_oauth2_clientcert';
   this.clientAssertionType = 'urn:ietf:params:oauth:client-assertion-type:jwt-bearer';
   this.pem = options.pem;
-  this.fingerprint = options.fingerprint;
 }
 
 util.inherits(Strategy, OAuth2Strategy);
@@ -19,9 +19,13 @@ Strategy.prototype.tokenParams = function (options) {
   var params = params || {};
   params['client_assertion_type'] = this.clientAssertionType;
 
+  const baseString = this.pem.value.match(/-----BEGIN CERTIFICATE-----\s*([\s\S]+?)\s*-----END CERTIFICATE-----/i);
+  const rawCert = Buffer.from(baseString[1], "base64");
+  const fingerprint = crypto.createHash("sha1").update(rawCert).digest("base64");
+
   var additionalHeaders = {
-      'x5t': this.fingerprint
-  }
+      'x5t': fingerprint
+  };
 
   const certJwt = {
       'aud': this._oauth2._accessTokenUrl,
@@ -31,13 +35,11 @@ Strategy.prototype.tokenParams = function (options) {
       'nbf': Math.floor(Date.now() / 1000) - 1000,
       'exp': Math.floor(Date.now() / 1000) + 7 * 8640000
   };
+
   // Sign the JWT with the PEM key
   const jwtToken = jwt.sign(certJwt, this.pem, { algorithm: 'RS256', header: additionalHeaders });
-  
   params['client_assertion'] = jwtToken;
-
   return params;
 }
-
 
 module.exports = Strategy;
